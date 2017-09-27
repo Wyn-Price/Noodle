a6725d3be593cae0fd2413b713714bec8ff8ad78
Wyn Price <wynprice999@gmail.com>
2017-09-27 08:38:44 +0100

    Fix #2

--- src/main/java/com/wynprice/noodle/NoodleWorldType.java
+++ src/main/java/com/wynprice/noodle/NoodleWorldType.java
@@ -2,8 +2,6 @@ package com.wynprice.noodle;
 
 import com.wynprice.noodle.generators.NoodleChunkGenerator;
 
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.GuiCreateWorld;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldType;
 import net.minecraft.world.gen.IChunkGenerator;
@@ -22,7 +20,7 @@ public class NoodleWorldType extends WorldType
 	}
 	
 	@Override
-	public void onCustomizeButton(Minecraft mc, GuiCreateWorld guiCreateWorld) {
+	public void onCustomizeButton(net.minecraft.client.Minecraft mc, net.minecraft.client.gui.GuiCreateWorld guiCreateWorld) {
 		mc.displayGuiScreen(new NoodleGuiCustomizeScreen(guiCreateWorld));
 	}
 	
